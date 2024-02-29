a25a29f6984c50ba04ec2d2a4fedd93309d1c401
Jonathan Jara <jothan_j@hotmail.com>
2024-02-28 22:56:25 -0300

    examen parcial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Form from './Componentes/Form'
+function App() {
+  let Marcas =  [{'Audi':'BMW','Mercedes-benz': 'Toyota','Ford': 'Chevrolet'}]
+
+
+  return (
+    <>
+  <Form></Form>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Form.jsx
@@ -0,0 +1,19 @@
+
+const Form = ()=> {   
+  return (
+    <div>
+        <H4>Marcas de autos</H4>
+        <form>
+        <label>Ingresa tu marca:</label>
+        <input type="text"/>
+        <label>Modelo:</label>
+        <input type="text"/>
+        <button>Buscar Marca</button>
+        </form>
+
+    </div>
+    
+  )
+}
+
+export default Form
